1fcef5d51df89c3025600c6103fcc9f41d7f4516
pratiks1010 <pratik10008sawant@gmail.com>
2023-09-19 12:41:58 +0530

    third commit

--- zomatoApp/zomato-cards/src/components/Footer .jsx
+++ zomatoApp/zomato-cards/src/components/Footer .jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import './Footer.css'; // Import the CSS file for styling
+import './Footer.css'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
-     const zomatologo =
+  const zomatologo =
     "https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png";
   return (
     <footer className="footer">
@@ -13,30 +15,30 @@ const Footer = () => {
         </div>
         <div className="footer-links">
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Menu</a></li>
-            <li><a href="#">Contact</a></li>
+            <li><a >Home</a></li>
+            <li><a >About Us</a></li>
+            <li><a >Menu</a></li>
+            <li><a >Contact</a></li>
           </ul>
         </div>
       </div>
       <div className="footer-details">
         <div className="footer-address">
           <h3>Our Address</h3>
-          <p>123 Zomato Street</p>
-          <p>Foodville, ZO 54321</p>
+          <p>Clover Park, Viman Nagar</p>
+          <p>Pune, Maharashtra 41101</p>
         </div>
         <div className="footer-contact">
           <h3>Contact Us</h3>
           <p>Email: info@zomato.com</p>
-          <p>Phone: +1 (123) 456-7890</p>
+          <p>Phone: +91558647784</p>
         </div>
       </div>
       <div className="footer-social">
         <ul>
-          <li><a href="#"><i className="fab fa-facebook"></i></a></li>
-          <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-          <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+          <li><a ><FontAwesomeIcon icon={faFacebook} /></a></li>
+          <li><a ><FontAwesomeIcon icon={faTwitter} /></a></li>
+          <li><a ><FontAwesomeIcon icon={faInstagram} /></a></li>
         </ul>
       </div>
     </footer>
